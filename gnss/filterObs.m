713ff67119dc092148b94ffece11bd6f6eb6b405
arnauochoa <arnau.ochoa-banuelos@csgroup.eu>
2021-05-12 14:58:31 +0200

    Small cleanup

--- gnss/filterObs.m
+++ gnss/filterObs.m
@@ -13,13 +13,4 @@ satVel(:, idxRmv) = [];
     apply_elevation_mask(obsGnss,rxPos,satPos,satClkBias,Config.ELEVATION_MASK);
 satClkDrift(idxMasked) = [];
 satVel(:, idxMasked) = [];
-
-% remove invalid pr? which thresholds ?? -> set as nan (like doppler)
-
-% remove doppler measurements with extremely large values
-% idxLrg = abs([obsGnss.D_Hz]) > Config.MAX_DOPPLER_MEAS | ...
-%     [obsGnss.D_sigma] > Config.MAX_DOPPLER_UNCERT;
-% if any(idxLrg) % Invalid dopplers are set to nan to maintain array size
-%     [obsGnss(idxLrg).D_Hz] = deal(nan);
-% end
 end
